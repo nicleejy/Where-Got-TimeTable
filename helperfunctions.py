54c08eac03ab55c2be1b0dc661d9de3b18872573
Guo Kecheng <guo.kecheng99@gmail.com>
2022-01-07 16:17:55 +0800

    helpers

--- helperfunctions.py
+++ helperfunctions.py
@@ -1,5 +1,6 @@
 import math
 from collections import namedtuple
+from main import *
 
 # Dictionaries to convert between days
 days = {"Monday": 0, "Tuesday": 1, "Wednesday": 2, "Thursday": 3, "Friday": 4, "Saturday": 5, "Sunday": 6}
@@ -10,7 +11,7 @@ VarClass = namedtuple("VarClass", ["module", "type", "no", "day", "start", "end"
 FixedClass = namedtuple("FixedClass", ["module", "type", "no", "day", "start", "end"]) #indicates the class slots must be fixed for indentification
 
 
-debugging = True
+debugging = False  
 
 # Sigmoid functions
 def sigmoid(x):
@@ -23,7 +24,7 @@ def dprint(item):
         print(item)
 
 
-# 
+# Checks for overlapping between x and y
 def range_overlapping(x, y): 
         if x.start == x.stop or y.start == y.stop: 
             return False 
@@ -43,6 +44,28 @@ def table_compressor(timetable):
             singlelist_timetable.append(each)
     sorted_timetable = sorted(singlelist_timetable, key = lambda row: (row[3], int(row[4])))
     return sorted_timetable
+
+
+# Takes a timetable array and reduces it to a nested dictionary for better parsing
+def parse_timetable(timetable_final):
+    all_classes = {}
+    for mod in module_codes:
+        mod_classes = {}
+        for session in timetable_final:
+            if mod == session.module:
+                if session.type.upper() == "TUTORIAL TYPE 2":
+                    mod_classes["TUT2"] = session.no
+                else:
+                    mod_classes[(session.type.upper())[0:3]] = session.no
+        all_classes[mod] = mod_classes
+    return all_classes
+
+# {'MA1521': {'TUT': '3', 'LEC': '1'}, 
+# 'CS1101S': {'TUT': '10D', 'LEC': '1', 'REC': '02A'}, 
+# 'ACC1701X': {'TUT': 'X15', 'LEC': 'X1'}, 
+# 'CS1231S': {'LEC': '1', 'TUT': '19A'}, 
+# 'ES2660': {'SEC': 'G09'}, 'IS1103': {}}
+
 
 # Generates an NUS Mods timetable link from a given timetable
 def generate_link(timetable):
